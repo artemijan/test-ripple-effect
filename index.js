c6d0b5cac132e61388b73d9ba0b246b5e2d059eb
artem <lineartem@gmail.com>
2016-02-24 18:48:44 +0200

    unhardcode ripple target

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
  */
 window._setRipplePosition = function (e) {
     //console.log(e.clientX);
-    e.currentTarget.children[0].style.left = -(parseInt(e.currentTarget.style.width) - e.layerX) + 'px';
-    e.currentTarget.children[0].style.top = -(parseInt(e.currentTarget.style.height) - e.layerY) + 'px';
+    e.currentTarget.getElementsByTagName('ripple-target')[0].style.left = -(parseInt(e.currentTarget.style.width) - e.layerX) + 'px';
+    e.currentTarget.getElementsByTagName('ripple-target')[0].style.top = -(parseInt(e.currentTarget.style.height) - e.layerY) + 'px';
 };
